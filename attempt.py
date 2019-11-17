0f22ae01075527525add207928c61f1fe1cf386a
Nesuki <49078396+Nesuki@users.noreply.github.com>
2019-11-16 22:30:12 -0300

    Update attempt.py

--- attempt.py
+++ attempt.py
@@ -1,24 +1,32 @@
-def info():
-	lista=[]
-	codigo=raw_input('Insert the product code: ')
-	while codigo<>"999":
-		descripcion=raw_input('Insert the product description: ')
-		precio_c=input('Insert the product cost: ')
-		precio_v=input('Insert the selling price of the product: ')
-		tipo=raw_input('Insert type of product (N if new or U if used): ')
-		lista.append([codigo,descripcion,precio_c,precio_v,tipo])
-		
-		codigo=raw_input('Insert the product code: ')
-		
-	return lista
-lista=info()
+def mainmenu():
+	print ('1. Add items to a list')
+	print ('2. Separate item based on New or Used status')
+	print ('3. Exit')
+	selection=int(input('Select choice: '))
+	if selection==1:
+		list()
+	elif selection==2:
+			list2()
+	elif selection==3:
+		quit()
+mainmenu()
+
+def list():
 	
+	lista=[]
+	code=raw_input('insert item id code')
+	while code<>"999":
+		description=raw_input('Insert item description')
+		price_cost=raw_input('Insert the product cost')
+		price_sell=raw_input('insert the selling price of the product')
+		item_type=raw_input('Insert type of product (N if new or U if used): ')
+		lista.append([code,description,price_cost,price_sell,item_type])
+		
+		code=raw_input('insert item id code')
+	return lista
 
-print ('Product management program')
-print ('----------------------------')
+lista=list()
 
-task = raw_input ('Select the task you want to do by entering a number')
+print lista
 
-if task == 1:
-	lista=info()
-
+mainmenu()
